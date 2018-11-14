475a07277eb61cf5804b1308738d71c666628508
Sanu Ann <sanuann@mit.edu>
2018-11-14 18:42:43 -0500

    remove uncommented code

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 export default {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/nda-phq9.jsonld',
-  contentSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/README.md',
-  //githubSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/feature_schema_ui/activities/PHQ-9/phq9_schema.jsonld?t=' + new Date().getMilliseconds(),
-  // githubSrc: 'https://raw.githubusercontent.com/akeshavan/schema-standardization/1c97ca6d857329fa247940c8f3c9699deb32f4cf/activities/PHQ-9/phq9_schema.jsonld',
-  // 'https://raw.githubusercontent.com/akeshavan/schema-standardization/1ceee3e312bed442056282f0e5bb6a60e1b29a55/activities/PHQ-9/phq9_schema.jsonld',
-  // 'https://raw.githubusercontent.com/akeshavan/schema-standardization/master/activities/PHQ-9/phq9_schema.jsonld',
-  // 'https://cdn.rawgit.com/akeshavan/schema-standardization/master/activities/PHQ-9/phq9_schema.jsonld',
+  contentSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/README.md'
 };
