a3c995feb742663c09bb02e612e9560e3de10801
Sanu Ann <sanuann@mit.edu>
2018-11-17 22:59:47 -0500

    Updated URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
   /* eslint-disable */
-  // githubSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/nda-phq9.jsonld',
-  // contentSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/README.md'
-  githubSrc: 'http://localhost:8080/static/json/activity-sets/example/nda-phq9.jsonld',
-  contentSrc: 'http://localhost:8080/static/json/activity-sets/example/README.md'
+  githubSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/nda-phq9.jsonld',
+  contentSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/master/activity-sets/example/README.md'
 };
