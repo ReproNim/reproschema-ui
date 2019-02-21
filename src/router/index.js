17b51fa55a183b593c9ba66a7c9c9135a0b27d8f
Anisha Keshavan <anisha@childmind.org>
2019-02-20 21:09:27 -0800

    rf: finished refactoring and pushed to bit

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Survey/';
 import Landing from '@/components/Landing/';
+import config from '../config';
 
 Vue.use(Router);
 
@@ -11,6 +12,9 @@ export default new Router({
       path: '/',
       name: 'Landing',
       component: Landing,
+      props: {
+        contentSrc: config.contentSrc,
+      },
     },
     {
       path: '/activities/:id',
