17d2e36bcf40027b64b3931ddde477f9b7df44df
Sanu Ann <sanuann@mit.edu>
2019-05-19 20:02:39 -0400

    change next question logic to what it was

--- src/components/Survey/Survey.vue
+++ src/components/Survey/Survey.vue
@@ -154,7 +154,7 @@ export default {
         // }
       }
       this.$forceUpdate();
-      if (idx <= this.listShow.length - 1) {
+      if (idx === this.listShow.length - 1) {
         const nextQuestionIdx = _.max(this.listShow) + 1;
         this.listShow.push(nextQuestionIdx);
         // update the listShow with the next index in case this one we added isn't visible
