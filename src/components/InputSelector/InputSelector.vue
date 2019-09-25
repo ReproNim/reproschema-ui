66f3ed0e426ac372a9b4cd130c478c788fd10285
Remi Gau <remi_gau@hotmail.com>
2019-09-25 13:36:35 +0200

    create input validation for floats

--- src/components/InputSelector/InputSelector.vue
+++ src/components/InputSelector/InputSelector.vue
@@ -69,7 +69,7 @@
           :init="init" v-on:valueChanged="sendData"/>
     </div>
 
-    <!-- If type is time rnage -->
+    <!-- If type is time range -->
     <div v-else-if="inputType==='timeRange'">
         <TimeRange
           :constraints="valueConstraints"
@@ -86,6 +86,14 @@
     <!-- If type is number -->
     <div v-else-if="inputType==='number'">
         <IntegerInput
+          :constraints="valueConstraints"
+          :selected_language="selected_language"
+          :init="init" v-on:valueChanged="sendData"/>
+    </div>
+
+    <!-- If type is float -->
+    <div v-else-if="inputType==='float'">
+        <FloatInput
           :constraints="valueConstraints"
           :selected_language="selected_language"
           :init="init" v-on:valueChanged="sendData"/>
@@ -159,6 +167,7 @@ import Radio from '../Inputs/WebRadio/';
 import AudioRecord from '../Inputs/WebAudioRecord/';
 import TextInput from '../Inputs/WebTextInput/';
 import IntegerInput from '../Inputs/WebIntegerInput/';
+import FloatInput from '../Inputs/WebFloatInput/';
 import DateInput from '../Inputs/YearInput/';
 import MultiPart from '../MultiPart/';
 import DocumentUpload from '../Inputs/DocumentUpload';
@@ -203,6 +212,7 @@ export default {
     AudioRecord,
     TextInput,
     IntegerInput,
+    FloatInput,
     DateInput,
     DocumentUpload,
     MultiTextInput,
---
+++ src/components/Inputs/WebFloatInput/FloatInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="FloatInput">
+    <b-form ref="form" @submit="onSubmit" :novalidate="true">
+      <b-form-group>
+        <b-form-input ref="floatInput" v-model="input"
+                      :class="{'is-invalid': !isValidFloat, 'is-valid': isValidFloat}">
+        </b-form-input>
+        <div class="invalid-feedback">
+          Please enter a number
+        </div>
+      </b-form-group>
+      <b-btn type="submit">Submit</b-btn>
+    </b-form>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+// import _ from 'lodash';
+// window._ = _;
+export default {
+  name: 'FloatInput',
+  props: ['constraints', 'init'],
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      this.$refs.form.className = 'was-validated';
+      if (this.isValidFloat) {
+        this.$emit('valueChanged', this.input);
+      }
+    },
+    isValid() {
+      // console.log(this.input, Number.isInteger(this.input));
+      const num = parseFloat(this.input);
+      if (isNaN(num)) {
+        return false;
+      }
+      return Number.isFinite(num);
+    },
+  },
+  computed: {
+    isValidFloat() {
+      return this.isValid(this.input);
+    },
+  },
+  data() {
+    return {
+      input: null,
+    };
+  },
+  mounted() {
+    if (this.init) {
+      this.input = this.init.value;
+    }
+  },
+};
+</script>
