07d3c15c72afb26761b7450b37c64dc81c14cf01
Sanu Ann <sanuann@mit.edu>
2020-03-26 00:58:45 -0400

    update landing page and change default protocol to Malargue

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/VoicePilot/VoicePilot_schema',
-  contentSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/VoicePilot/README.md'
+  githubSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/Malargue/Malargue_schema',
+  contentSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/Malargue/README.md'
 };
