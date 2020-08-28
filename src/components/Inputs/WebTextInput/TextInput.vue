2931819e02d9d26cc3d6e9068c3fb645bb52423f
Sanu Ann <sanuann@mit.edu>
2020-08-28 19:55:17 -0400

    support for structured output

--- src/components/Inputs/WebTextInput/TextInput.vue
+++ src/components/Inputs/WebTextInput/TextInput.vue
@@ -1,37 +1,98 @@
 <template>
   <div class="textInput">
-    <b-form @submit="onSubmit">
-      <b-form-group>
-        <b-form-input v-model="input">
-        </b-form-input>
-      </b-form-group>
+    <b-form ref="form" @submit="onSubmit">
+      <b-row>
+        <b-col lg="4" class="col-4 col-md-4">
+          <b-form-group>
+            <b-form-input type="text" v-model="input">
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col lg="4" v-if="hasUnit && Array.isArray(options)">
+          <multiselect v-model="input2"
+                       :options="this.options" :searchable="false" :show-labels="false"
+                       :allowEmpty="false" placeholder="unit">
+          </multiselect>
+        </b-col>
+        <div v-else-if="hasUnit && !Array.isArray(options)">
+          <p class="singleUnit"> {{ options }}</p>
+        </div>
+      </b-row>
       <b-btn type="submit">Submit</b-btn>
     </b-form>
   </div>
 </template>
 
 <style>
+  .multiselect__tags {
+    padding: 4px 40px 0 15px!important;
+    border: 1px solid #ced4da;
+    max-height: 38px;
+    min-height: 38px;
+  }
+
+  .multiselect__placeholder {
+    color: #6c757c;
+    font-size: 1rem;
+  }
+
+  .multiselect__single {
+    margin-top: 5px;
+  }
+
+  .singleUnit {
+    margin-top: 0.5rem;
+  }
+
 </style>
 
 <script>
+import _ from 'lodash';
+import Multiselect from 'vue-multiselect';
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+
 export default {
   name: 'TextInput',
   props: ['constraints', 'init', 'inputType', 'selected_language'],
-  methods: {
-    onSubmit(e) {
-      e.preventDefault();
-      this.$emit('valueChanged', this.input);
-    },
+  components: {
+    Multiselect,
   },
   data() {
     return {
-      input: '',
+      input1: '',
+      input2: '',
     };
   },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      this.$refs.form.className = 'was-validated';
+      if (this.hasUnit) { // send value + unit
+        const name = { value: this.input1, unitCode: this.input2 };
+        this.$emit('valueChanged', name);
+      } this.$emit('valueChanged', this.input1); // else send only value
+    },
+  },
+  computed: {
+    hasUnit() {
+      if (this.constraints['http://schema.org/unitCode']) {
+        return true;
+      } return false;
+    },
+    options() {
+      if (this.constraints['http://schema.org/unitCode'].length > 1) {
+        return _.map(this.constraints['http://schema.org/unitCode'], unit => unit['@value']);
+      } else if (this.constraints['http://schema.org/unitCode'].length === 1) {
+        return this.constraints['http://schema.org/unitCode'][0]['@value'];
+      } return ''; // no unit present
+    },
+  },
   mounted() {
-    console.log(32, 'in text', this.inputType);
     if (this.init) {
-      this.input = this.init;
+      if (this.hasUnit) {
+        this.input1 = this.init.value;
+        this.input2 = this.init.unitCode;
+      } else this.input1 = this.init;
     }
   },
 };
