0f5653a64ca821dd58869d6453dab65ae8ffacdf
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2020-12-28 19:53:20 -0500

    test removing banner message

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/VoicePilot/VoicePilot_schema',
-  banner: {
-    "en": 'This service is a demonstration for the ReproNim project.',
-    "es": 'Este servicio es una demostración para el proyecto ReproNim.'
-  },
   startButton: {
     "en": 'Join',
     "es": 'Participar'
