c2776a050fdc1db98edff466314a47adb8287cc4
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2021-03-02 14:11:14 -0500

    update timeout to 2mins

--- src/components/Inputs/SaveData/SaveData.vue
+++ src/components/Inputs/SaveData/SaveData.vue
@@ -150,7 +150,7 @@ export default {
               this.percentCompleted = parseInt(Math.round( (progressEvent.loaded * 100) / progressEvent.total ));
             }.bind(this),
             'Content-Type': 'multipart/form-data',
-            timeout: 3000
+            timeout: 120000
           };
           axios.post(`${config.backendServer}/submit`, formData, config1).then((res) => {
               this.hasData = true;
