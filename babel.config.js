19f942ec9255ef5f6d163bc2b2c4ab2dafac68ab
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2021-10-22 17:05:42 +0100

    Unit tests NOT working

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    '@vue/cli-plugin-babel/preset'
+  ];
+
+  const plugins = [];
+
+  // Mock require.context() calls for jest to prevent crashes
+  // https://stackoverflow.com/a/68297118
+  if (process.env.NODE_ENV === 'test') {
+    plugins.push('transform-require-context');
+  }
+
+  return {
+    presets,
+    plugins
+  };
 };
