e2576a0e4498cba39ddb3a1f8d491e20918982dd
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2021-10-22 16:38:08 +0100

    Working e2e test example
    Added missing dependency to `package.json` and changed test target text to text present on the homepage.

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,12 @@
+// Cypress end2end testing will open in its own mini-application. When it does,
+// click on a test or on 'run all specs' to pop open a browser instance that
+// will open the app and interact with it according to the instructions in the
+// body of the test.
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("This service is a demonstration for the ReproNim project.");
+    cy.contains("This is a Demonstration Protocol.");
   });
 });
