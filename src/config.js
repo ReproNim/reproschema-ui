4ce97fa214d31b2782817fd270b45b5911c45eb8
Satrajit Ghosh <satra@mit.edu>
2024-04-04 23:13:30 -0400

    restore default protocol

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  githubSrc: 'https://raw.githubusercontent.com/satra/nimh-minimal/main/nimh_minimal/nimh_minimal_schema',
+  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
   startButton: {
     "en": 'Join',
     "es": 'Participar'
