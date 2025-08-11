684716e797d52545ffed7f18456dd650ec076778
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2025-08-11 15:13:09 -0400

    added envrionment variable to simplify switching schemas in UI

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/main/DemoProtocol/DemoProtocol_schema',
+  githubSrc: process.env.VUE_APP_REPROSCHEMA_GITHUB_SRC || 'https://raw.githubusercontent.com/ReproNim/demo-protocol/main/DemoProtocol/DemoProtocol_schema',
   startButton: {
     "en": 'Join',
     "es": 'Participar'
@@ -11,3 +11,5 @@ module.exports = {
   contact: 'voice_survey@mit.edu',
   emailSubject: 'Help with Covid19 study'
 };
+
+console.log(`Loading schema from REPROSCHEMA_GITHUB_SRC: ${process.env.VUE_APP_REPROSCHEMA_GITHUB_SRC}`);
